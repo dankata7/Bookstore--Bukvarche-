7ad3dd638374a27cc5d6951010ac30026a2c6a33
dankata7 <d.vladimirov2017@pmg-pernik.com>
2022-02-28 16:30:35 +0200

    today update site

---
+++ Bookstore-Bukvarche/Bookstore-Bukvarche/Domain/RegistratedUsers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore_Bukvarche.Domain
+{
+    public class RegistratedUsers
+    {
+    }
+}
